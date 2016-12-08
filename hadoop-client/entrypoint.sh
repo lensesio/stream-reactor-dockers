3588fb606458adf40c410f888d338742945e8ad0
Andrew Stevenson <andrew@datamountaineer.com>
2016-12-08 15:33:46 +0100

    fix confluent

--- hadoop-client/entrypoint.sh
+++ hadoop-client/entrypoint.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
 
-# Pull cloudera config
-echo "Getting Hadoop client config"
-curl $CLOUDERA_MANAGER_URL/api/v12/clusters/$CLOUDERA_CLUSTER_NAME/services/$YARN_SERVICE_NAME/clientConfig >> /etc/hadoop-conf.zip
-(cd /etc && unzip hadoop-conf.zip)
-(cd /etc/hadoop-conf && sed -i 's/.engd.local//g' *)
+echo "Getting yarn client config"
+curl $CLOUDERA_MANAGER_URL/api/v12/clusters/$CLOUDERA_CLUSTER_NAME/services/YARN/clientConfig >> /etc/yarn-conf.zip || exit 1
+(cd /etc && unzip yarn-conf.zip)
 
-echo "Getting Hive client config"
-curl $CLOUDERA_MANAGER_URL/api/v12/clusters/$CLOUDERA_CLUSTER_NAME/services/$HIVE_SERVICE_NAME/clientConfig >> /etc/hive-conf.zip
+echo "Getting hive client config"
+curl $CLOUDERA_MANAGER_URL/api/v12/clusters/$CLOUDERA_CLUSTER_NAME/services/HIVE/clientConfig >> /etc/hive-conf.zip || exit 1
 (cd /etc && unzip hive-conf.zip)
-(cd /etc/hive-conf && sed -i 's/.engd.local//g' *)
-
-
-echo "Getting HBase client config"
-curl $CLOUDERA_MANAGER_URL/api/v12/clusters/$CLOUDERA_CLUSTER_NAME/services/$HBASE_SERVICE_NAME/clientConfig >> /etc/hbase-conf.zip
-(cd /etc && unzip hbase-conf.zip)
-(cd /etc/hbase-conf && sed -i 's/.engd.local//g' *)
-
-# echo "Getting spark client config"
-# curl $CLOUDERA_MANAGER_URL/api/v12/clusters/$CLOUDERA_CLUSTER_NAME/services/SPARK/clientConfig >> /etc/spark-conf.zip
-# (cd /etc && unzip spark-conf.zip)
-# (cd /etc/spark-conf && sed -i 's/.engd.local//g' *)
